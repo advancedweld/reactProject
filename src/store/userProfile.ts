8a533728ab23582e2193bc9a2560d79cb929ce02
engineMaster <xiangshangzhi@gmail.com>
2023-08-05 15:28:38 +0800

    feat: zustand数据持久化

--- src/store/userProfile.ts
+++ src/store/userProfile.ts
@@ -1,9 +1,18 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const useUserProfileStore = create((set) => ({
-  isLogin: false,
-  login: () => set((state: any) => ({ isLogin: true })),
-  logout: () => set((state: any) => ({ isLogin: false })),
-}))
+const useUserProfileStore = create(
+  persist(
+    (set) => ({
+      isLogin: false,
+      login: () => set((state: any) => ({ isLogin: true })),
+      logout: () => set((state: any) => ({ isLogin: false })),
+    }),
+    {
+      name: 'userProfile-storage', // unique name
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+)
 
 export default useUserProfileStore
