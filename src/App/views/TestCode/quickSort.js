8cac666d62a90180f9d31188eac1c4df33cecafb
xiangshangzhi <xiangshangzhi@163.com>
2023-12-12 14:42:32 +0800

    feat: usestate和useeffect实现原理

--- src/App/views/TestCode/quickSort.js
+++ src/App/views/TestCode/quickSort.js
@@ -3,7 +3,7 @@
  * @Author: xiangshangzhi xiangshangzhi@163.com
  * @Date: 2023-05-12 13:37:38
  * @LastEditors: xiangshangzhi xiangshangzhi@163.com
- * @LastEditTime: 2023-12-04 16:31:32
+ * @LastEditTime: 2023-12-06 15:44:16
  * @FilePath: \reactProject\src\App\views\TestCode\quickSort.js
  * @Description: 快速排序
  *
@@ -16,6 +16,7 @@ const quickSort = (arr) => {
   let left = []
   let mid = arr[0]
   let right = []
+  // 注意从第一个元素开始遍历，第0个元素是基准元素
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < mid) {
       left.push(arr[i])
@@ -26,6 +27,6 @@ const quickSort = (arr) => {
   return [...quickSort(left), mid, ...quickSort(right)]
 }
 
-let arr = [1, 2, 3, 4, 55, 33, 65, 3, 4, 5, 6, 223, 6778, 4, 324, 657, 34, 7655]
+let arr = [1, 2, 3, 4, 55, 33, 65, 3, 4, 5, 6, 223, 4, 324, 34]
 
-console.log(quickSort(arr))
+console.log(quickSort(arr)) // [1, 2, 3, 4, 5, 6, 33, 34, 55, 65, 223, 324]
