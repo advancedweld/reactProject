b79729d02c5e8d200b7dce4e3141bbb089694852
xiangshangzhi <xiangshangzhi@163.com>
2023-12-21 17:44:12 +0800

    feat: init用户登录

--- src/App/views/Login/index.tsx
+++ src/App/views/Login/index.tsx
@@ -1,32 +1,39 @@
 /*
  * @Author: xiangshangzhi xiangshangzhi@163.com
  * @Date: 2023-04-03 19:13:51
- * @LastEditors: engineMaster xiangshangzhi@gmail.com
- * @LastEditTime: 2023-12-09 15:42:49
+ * @LastEditors: xiangshangzhi xiangshangzhi@163.com
+ * @LastEditTime: 2023-12-21 17:43:29
  * @FilePath: \reactProject\src\App\views\Login\index.tsx
  * @Description: xiangshangzhi写的文件
  *
  */
 import React, { useEffect } from 'react'
 import { Button, message, Form, Input } from 'antd'
+import { useQuery, useMutation } from '@tanstack/react-query'
 
 import { useNavigate } from 'react-router-dom'
 import { APP } from 'routes/constant'
-
 import useUserProfileStore from 'store/userProfile'
+
+import { userLogin } from './service/api'
 
 import style from './style.module.css'
 
 function Login() {
-  // const loginStatus = useUserProfileStore((state) => state.isLogin)
-  // const login = useUserProfileStore((state) => state.login)
-  // const logout = useUserProfileStore((state) => state.logout)
-
   const { userName, logout, changeUserName, login } = useUserProfileStore((state) => state)
 
   const [form] = Form.useForm<any>()
 
   const nav = useNavigate()
+
+  // 用户登录请求
+  const userLoginMutation = useMutation({
+    mutationFn: userLogin,
+    onSuccess: () => {
+      nav(APP)
+      message.success('登录成功')
+    },
+  })
 
   const onFinish = async (values: any) => {
     console.log('@@@onFinish values is -----', values)
---
+++ src/App/views/Login/service/api.ts
@@ -0,0 +1,7 @@
+import request from 'utils/request'
+import { User } from './type'
+
+// 获取验证码
+export const userLogin: (params: User) => Promise<string> = (params) =>
+  // request.post('/user/getCode', { phone })
+  request.post('/api-x/user/login', params, { timeout: 0 })
