63920f26b02e7b9fa2b67c9a943e9a648150eb91
xiangshangzhi <xiangshangzhi@163.com>
2023-12-21 11:29:59 +0800

    feat: 粒子效果

--- src/App/views/MyCollect/index.tsx
+++ src/App/views/MyCollect/index.tsx
@@ -4,23 +4,62 @@
  * @FilePath: \reactProject\src\App\views\MyCollect\index.tsx
  * @Description: xiangshangzhi写的文件
  * @LastEditors: xiangshangzhi xiangshangzhi@163.com
- * @LastEditTime: 2023-12-21 10:20:30
+ * @LastEditTime: 2023-12-21 11:27:31
  *
  */
 import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Button } from 'antd'
 
 import style from './style.module.css'
 
 function Entry() {
-  const location = useLocation()
-  useEffect(() => {
-    console.log('@@location in login is -----', location)
-  })
+  function pop(evt: React.MouseEvent<HTMLAnchorElement, MouseEvent> | React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log('@@@@evt', evt)
+    for (let i = 0; i < 30; i++) {
+      createParticle(evt.clientX, evt.clientY)
+    }
+  }
+
+  function createParticle(x: number, y: number) {
+    console.log('@@@@@@createParticle', x, y)
+    const particle = document.createElement('div')
+    particle.classList.add('particle')
+    const size = Math.floor(Math.random() * 20 + 5)
+    particle.style.width = `${size}px`
+    particle.style.height = `${size}px`
+    particle.style.background = `hsl(${Math.random() * 90 + 180}, 70%, 60%)`
+    const destinationX = x + (Math.random() - 0.5) * 2 * 75
+    const destinationY = y + (Math.random() - 0.5) * 2 * 75
+    const animation = particle.animate(
+      [
+        {
+          transform: `translate(${x - size / 2}px, ${y - size / 2}px)`,
+          opacity: 1,
+        },
+        {
+          transform: `translate(${destinationX}px, ${destinationY}px)`,
+          opacity: 0,
+        },
+      ],
+      {
+        duration: 500 + Math.random() * 1000,
+        easing: 'cubic-bezier(0, .9, .57, 1)',
+        delay: Math.random() * 200,
+      },
+    )
+    animation.onfinish = () => {
+      particle.remove()
+    }
+    document.body.appendChild(particle)
+  }
+
   return (
     <>
       <div className={style.wrap}>
         <h1> 我的收藏 </h1>
+        <Button type='primary' onClick={(evt) => pop(evt)}>
+          点击我
+        </Button>
       </div>
     </>
   )
