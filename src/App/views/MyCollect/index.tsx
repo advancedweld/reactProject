82260ab0860b0f4755557d40cebf5da2d1381c82
xiangshangzhi <xiangshangzhi@163.com>
2023-12-21 11:45:53 +0800

    feat：粒子效果

--- src/App/views/MyCollect/index.tsx
+++ src/App/views/MyCollect/index.tsx
@@ -2,9 +2,9 @@
  * @Author: xiangshangzhi xiangshangzhi@163.com
  * @Date: 2023-12-21 10:17:57
  * @FilePath: \reactProject\src\App\views\MyCollect\index.tsx
- * @Description: xiangshangzhi写的文件
+ * @Description: 粒子效果 参考 https://juejin.cn/post/7303718687235768357
  * @LastEditors: xiangshangzhi xiangshangzhi@163.com
- * @LastEditTime: 2023-12-21 11:27:31
+ * @LastEditTime: 2023-12-21 11:45:06
  *
  */
 import React, { useEffect } from 'react'
@@ -13,13 +13,6 @@ import { Button } from 'antd'
 import style from './style.module.css'
 
 function Entry() {
-  function pop(evt: React.MouseEvent<HTMLAnchorElement, MouseEvent> | React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    console.log('@@@@evt', evt)
-    for (let i = 0; i < 30; i++) {
-      createParticle(evt.clientX, evt.clientY)
-    }
-  }
-
   function createParticle(x: number, y: number) {
     console.log('@@@@@@createParticle', x, y)
     const particle = document.createElement('div')
@@ -27,9 +20,13 @@ function Entry() {
     const size = Math.floor(Math.random() * 20 + 5)
     particle.style.width = `${size}px`
     particle.style.height = `${size}px`
-    particle.style.background = `hsl(${Math.random() * 90 + 180}, 70%, 60%)`
-    const destinationX = x + (Math.random() - 0.5) * 2 * 75
-    const destinationY = y + (Math.random() - 0.5) * 2 * 75
+    // particle.style.background = `hsl(${Math.random() * 90 + 180}, 70%, 60%)`
+
+    particle.style.background = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255},${Math.random()})`
+    const destinationX = x + (Math.random() - 0.5) * 2 * 90
+    const destinationY = y + (Math.random() - 0.5) * 2 * 90
+    // 纵向不散开
+    // const destinationY = y
     const animation = particle.animate(
       [
         {
@@ -38,11 +35,12 @@ function Entry() {
         },
         {
           transform: `translate(${destinationX}px, ${destinationY}px)`,
-          opacity: 0,
+          opacity: 0.1,
+          // opacity: 0,
         },
       ],
       {
-        duration: 500 + Math.random() * 1000,
+        duration: 1000 + Math.random() * 1000,
         easing: 'cubic-bezier(0, .9, .57, 1)',
         delay: Math.random() * 200,
       },
@@ -53,13 +51,15 @@ function Entry() {
     document.body.appendChild(particle)
   }
 
+  const handleGenerateStar = (evt: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    for (let i = 0; i < 100; i++) {
+      createParticle(evt.clientX, evt.clientY)
+    }
+  }
   return (
     <>
-      <div className={style.wrap}>
-        <h1> 我的收藏 </h1>
-        <Button type='primary' onClick={(evt) => pop(evt)}>
-          点击我
-        </Button>
+      <div className={style.wrap} onClick={(evt) => handleGenerateStar(evt)}>
+        <h1> 点击触发粒子效果 </h1>
       </div>
     </>
   )
