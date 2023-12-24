c8e9107b1ddd6c0a91020fbaa542fcf648140bfe
engineMaster <xiangshangzhi@gmail.com>
2023-12-24 15:12:05 +0800

    feat: 加默认账号密码

--- src/App/views/Login/components/LoginForm.tsx
+++ src/App/views/Login/components/LoginForm.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: xiangshangzhi xiangshangzhi@163.com
  * @Date: 2023-04-03 19:13:51
- * @LastEditors: xiangshangzhi xiangshangzhi@163.com
- * @LastEditTime: 2023-12-22 16:40:49
+ * @LastEditors: engineMaster xiangshangzhi@gmail.com
+ * @LastEditTime: 2023-12-24 15:11:22
  * @FilePath: \reactProject\src\App\views\Login\components\LoginForm.tsx
  * @Description: xiangshangzhi写的文件
  *
@@ -15,6 +15,10 @@ import style from '../style.module.css'
 interface ILoginForm {
   onFinish: (values: any) => void
 }
+type FormType = {
+  userName: string
+  password: string
+}
 function LoginForm(props: ILoginForm) {
   const [form] = Form.useForm<any>()
 
@@ -24,16 +28,17 @@ function LoginForm(props: ILoginForm) {
 
       <Form
         form={form}
+        initialValues={{ userName: 'admin', password: '123456' }}
         onFinish={props.onFinish}
         name={'loginForm'}
         style={{ marginTop: '8px' }}
         labelCol={{ flex: '50px' }}
         labelAlign='left'
         autoComplete='off'>
-        <Form.Item label='账号' name='userName' rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Form.Item<FormType> label='账号' name='userName' rules={[{ required: true, message: 'Please input your username!' }]}>
           <Input style={{ height: '40px' }} />
         </Form.Item>
-        <Form.Item label='密码' name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
+        <Form.Item<FormType> label='密码' name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
           <Input style={{ height: '40px' }} type='password' />
         </Form.Item>
 
