487ccbabad6f1731249ba0cb7f2487f22bb6cac9
xiangshangzhi <xiangshangzhi@163.com>
2023-12-29 10:45:19 +0800

    feat: 替换登录接口

--- src/App/views/Login/service/api.ts
+++ src/App/views/Login/service/api.ts
@@ -4,8 +4,8 @@ import { User, UserRegisterResponse, UserLoginResponse } from './type'
 // 用户登录
 export const userLogin: (params: User) => Promise<UserLoginResponse> = (params) =>
   // request.post('/user/getCode', { phone })
-  request.post('/api-x/user/login', params, { timeout: 0 })
-
+  // request.post('/api-x/user/login', params, { timeout: 0 })
+  request.post('/api-x/auth/login', params, { timeout: 0 })
 // 用户注册
 export const userRegister: (params: User) => Promise<UserRegisterResponse> = (params) =>
   // request.post('/user/getCode', { phone })
