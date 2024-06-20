1785a9b54ef0c05a5f9bf8896e340ce5b35689b3
xiangshangzhi <xiangshangzhi@163.com>
2024-06-20 09:42:47 +0800

    feat: 数据上报

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
  * @Author: xiangshangzhi xiangshangzhi@xtfer.com
  * @Date: 2022-07-20 12:55:04
  * @LastEditors: xiangshangzhi xiangshangzhi@163.com
- * @LastEditTime: 2024-06-19 17:10:05
+ * @LastEditTime: 2024-06-19 19:10:00
  * @FilePath: \reactProject\src\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ import dayjs from 'dayjs'
 
 dayjs.locale('zh-cn')
 
-const eagle = new Eagle({ piggyName: 'lihuahah', interval: 5000, monitorRequest: true, monitorApi: 'http://localhost:3000/api-x/user/register' })
+const eagle = new Eagle({ piggyName: 'lihuahah', interval: 5000, monitorRequest: true, monitorApi: 'http://localhost:3000/api-x/stastics' })
 window.onload = () => {
   console.log('window.onload')
   eagle.startMonitoring()
