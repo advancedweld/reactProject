b07328c82ad09e9f11c2785e060bfdfcbe85db93
xiangshangzhi <xiangshangzhi@163.com>
2024-08-14 11:06:32 +0800

    feat: react批处理demo

--- src/App/views/Components/index.tsx
+++ src/App/views/Components/index.tsx
@@ -10,10 +10,31 @@ function Login() {
   const [count1, setCount1] = useState(1)
   const [count2, setCount2] = useState(1)
 
+  const [batchCount, setBatchCount] = useState(0)
+  const handleBatchCountWithFun = () => {
+    /**
+     * 传递函数能实时拿到最新的state
+     * 同时会将多次的操作合并为一次更新，只触发一次
+     */
+    setBatchCount((pre) => pre + 1)
+    setBatchCount((pre) => pre + 2)
+  }
+
+  const handleBatchCount = () => {
+    /**
+     * 这里会形成闭包，捕获batchCount
+     * 因此这里两次后的结果是1+2为3
+     */
+    setBatchCount(batchCount + 1)
+    setBatchCount(batchCount + 2)
+  }
+  useEffect(() => {
+    console.log('@@@组件更新batch', batchCount)
+  })
   return (
     <>
       <div className={style.wrap}>
-        <input value={'shuru'}></input>
+        <input defaultValue={'shuru'}></input>
         <Button>按钮</Button>
         <Input.Search value='搜索哈哈' prefix={<span>ttg</span>}></Input.Search>
       </div>
@@ -21,6 +42,12 @@ function Login() {
       <Button onClick={() => setCount2(count2 + 1)}>count2++</Button>
       <Button onClick={() => setCount(count + 1)}>count++</Button>
       <h1>{`父组件---${count}`}</h1>
+      <div>
+        <h1>{`setcount批量处理: ${batchCount}`}</h1>
+        <Button onClick={handleBatchCount}>count++</Button>
+        <Button onClick={handleBatchCountWithFun}>fun count++</Button>
+      </div>
+
       <ChildCom count={count1} name='组件1'></ChildCom>
       <ChildCom count={count2} name='组件2'></ChildCom>
     </>
